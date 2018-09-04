0cfacc73cb43349bfcbcdb725c4b4a0398487bbc
Nicolas Hermet <n.hermet@gmail.com>
2018-09-04 22:27:10 +0200

    fix bug on multiple users devise accounts

--- app/controllers/concerns/accessible.rb
+++ app/controllers/concerns/accessible.rb
@@ -1,19 +1,23 @@
 module Accessible
   extend ActiveSupport::Concern
   included do
-    before_action :check_artist
+    before_action :check_user
   end
 
   protected
-  def check_artist
-    if current_admin
+  def check_user
+    if current_mecene
       flash.clear
       # if you have rails_admin. You can redirect anywhere really
-      redirect_to(rails_admin.dashboard_path) && return
+      redirect_to(root_path) && return
     elsif current_artist
       flash.clear
       # The authenticated root path can be defined in your routes.rb in: devise_scope :user do...
-      redirect_to(authenticated_artist_root_path) && return
+      redirect_to(root_path) && return
+    # elsif current_admin
+    #   flash.clear
+    #   # if you have rails_admin. You can redirect anywhere really
+    #   redirect_to(rails_admin.dashboard_path) && return
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-	devise_for :mecenes, path: 'mecenes', controllers: { sessions: "mecenes/sessions"}
-	devise_for :artists, path: 'artists', controllers: { sessions: "artists/sessions"}
+	devise_for :mecenes, path: 'mecenes', controllers: { sessions: "mecenes/sessions", registrations: "mecenes/registrations"}
+	devise_for :artists, path: 'artists', controllers: { sessions: "artists/sessions", registrations: "artists/registrations"}
 	
 	root 'home#index'
 
-	get 'home/index'
+	# get 'home/index'
 end
