12a1358828037c470ac1947014a09ba1daaf4c9a
Nicolas Hermet <n.hermet@gmail.com>
2018-09-04 09:58:19 +0200

    add controller, views, config and routes for artist

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :artists, path: 'artists'
+  devise_for :artists, path: 'artists', controllers: { sessions: "artists/sessions"}
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
