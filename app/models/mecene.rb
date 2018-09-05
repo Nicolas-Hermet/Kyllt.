956b61d5b5610cfc1eadf978a88d686c8ec57df1
ldelestre <l98.broadhurst@gmail.com>
2018-09-05 10:49:43 +0200

    validations field first commit

--- app/models/mecene.rb
+++ app/models/mecene.rb
@@ -1,11 +1,32 @@
 class Mecene < ApplicationRecord
-	# Include default devise modules. Others available are:
-	# :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 	devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-	validates :first_name, presence: true
-	validates :last_name, presence: true 
-	validates :gcs_read, acceptance: true
-
+	
 	has_many :buffers
 	has_many :projects, {:through => :buffers, :source=>"project"}
+
+	validates :email, presence: true, uniqueness: { case_sensitive: false }
+	validates_format_of :email, :with => /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+	validates :first_name, presence: true
+	validates_format_of :first_name, :with => /^[a-zA-Z]+$/, :multiline => true
+
+	validates :last_name, presence: true
+	validates_format_of :last_name, :with => /^[a-zA-Z]+$/, :multiline => true
+
+	validates :min_budget, length: { minimum: 3 }, on: :create, allow_blank: true
+	validates_format_of :min_budget, :with => /^[0-9]+$/, :multiline => true, on: :create, allow_blank: true
+
+	validates :max_return_invest, length: { maximum: 50 }, on: :create, allow_blank: true
+	validates_format_of :max_return_invest, :with => /^[A-Za-z0-9_-]+$/, :multiline => true, on: :create, allow_blank: true
+
+	validates :art_category, length: { maximum: 50 }, on: :create, allow_blank: true
+	validates_format_of :art_category, :with => /^[A-Za-z0-9_-]+$/, :multiline => true, on: :create, allow_blank: true
+
+	validates :locality, length: { maximum: 5 }, on: :create, allow_blank: true
+	validates_format_of :locality, :with => /^[0-9_-]+$/, :multiline => true, on: :create, allow_blank: true
+
+	validates :iban, length: { maximum: 33 }, on: :create, allow_blank: true
+	validates_format_of :iban, :with => /^[A-Za-z0-9_-]+$/, :multiline => true, on: :create, allow_blank: true
+
+	validates :gcs_read, acceptance: true
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,30 @@
 class Project < ApplicationRecord
 	belongs_to :artist
+
 	has_many :buffers
 	has_many :mecenes, through: :buffers
+
+	validates :title, presence: true, length: { maximum: 200 }
+	validates_format_of :title, :with => /^[A-Za-z0-9_-]+$/, :multiline => true
+
+	validates :explanation, presence: true, length: { maximum: 10000 }
+	validates_format_of :explanation, :with => /^[A-Za-z0-9_-]+$/, :multiline => true
+
+	validates :category, presence: true, length: { maximum: 50 }
+	validates_format_of :category, :with => /^[A-Za-z0-9_-]+$/, :multiline => true
+
+	validates :duration, presence: true, length: { maximum: 10 }
+	validates_format_of :duration, :with => /^[A-Za-z0-9_-]+$/, :multiline => true
+
+	validates :budget, presence: true
+	validates_format_of :budget, :with => /^[0-9_-]+$/, :multiline => true
+
+	validates :funding, presence: true
+	validates_format_of :funding, :with => /^[0-9_-]+$/, :multiline => true
+
+	validates :duration, presence: true, length: { maximum: 50 }
+	validates_format_of :duration, :with => /^[A-Za-z0-9_-]+$/, :multiline => true
+
+	validates :scope, presence: true, length: { maximum: 2500 }
+	validates_format_of :scope, :with => /^[A-Za-z0-9_-]+$/, :multiline => true
 end
