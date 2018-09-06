67c14287016b029fff0fab4256ebfbda1bef4cb6
ldelestre <l98.broadhurst@gmail.com>
2018-09-06 09:27:45 +0200

    controller + form updated

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,14 +1,15 @@
 class ProjectsController < ApplicationController
+	def index
+		@project = Project.all
+	end
+
 	def new
 	end
 
 	def create
-		@project = Project.new(project_params)
+		@project = Project.create(project_params)
 	end
-
-	private
-
-	def project_params
-	params.require(:project).permit(:title, :description, :category, :duration, :budget, :show_fav_place, :scope, :innovative)
+	def create
+		project = User.create(title: params[:project][:title], description: params[:project][:description], category: params[:project][:category], duration: params[:project][:duration], budget: params[:project][:budget], show_fav_place: params[:project][:show_fav_place], scope: params[:project][:scope], innovative: params[:project][:innovative])
 	end
 end
