b4246151dd06e8c3c9ba1be5fdb47757608d879b
Nicolas Hermet <n.hermet@gmail.com>
2018-09-12 12:07:13 +0200

    add list of funded project in mecene profile

--- app/controllers/mecenes_controller.rb
+++ app/controllers/mecenes_controller.rb
@@ -3,5 +3,14 @@ class MecenesController < ApplicationController
 	end
 
 	def show
+		@buffer = Buffer.where(mecene: current_mecene)
+		@project = Project.all
+		@arr = Hash.new
+		@buffer.each do |b|
+			@arr[@project.find(b.project_id)] = 0
+		end
+		@buffer.each do |b|
+			@arr[@project.find(b.project_id)] += b.invest
+		end
 	end
 end
