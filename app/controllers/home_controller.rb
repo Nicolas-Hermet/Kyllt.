b7bc3722db4807cb80417a1eab27968642d9a9fe
ldelestre <l98.broadhurst@gmail.com>
2018-09-13 18:05:10 +0200

    updated simulation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,8 @@
 class HomeController < ApplicationController
 	def simulate
         if !params[:simu].nil?
-            binding.pry
             @result = params[:simu][:investissement].to_f/params[:simu][:budget].to_f*0.45*params[:simu][:vente].to_f
             @part_societe = params[:simu][:investissement].to_f/params[:simu][:budget].to_f*0.45*100
-            # redirect_to simulation_path
         end
     end
 
