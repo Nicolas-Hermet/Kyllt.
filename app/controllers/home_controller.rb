bdf992ab96398d76642b7dffac14b57b9a249844
ldelestre <l98.broadhurst@gmail.com>
2018-09-13 17:50:58 +0200

    binding pry removed in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,10 @@
 class HomeController < ApplicationController
 	def simulate
-        if !params[:simu].nil?
-            binding.pry
+		if !params[:simu].nil?
             @result = params[:simu][:investissement].to_f/params[:simu][:budget].to_f*0.45*params[:simu][:vente].to_f
             @part_societe = params[:simu][:investissement].to_f/params[:simu][:budget].to_f*0.45*100
-            # redirect_to simulation_path
-        end
-    end
+		end
+	end
 
 
     def index
