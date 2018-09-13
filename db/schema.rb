618051a41fe455bc927eac74e31d5026ac6f53eb
Lvmmbrkln <j.degueltzl@gmail.com>
2018-09-13 12:16:08 +0200

    index view

--- db/schema.rb
+++ db/schema.rb
@@ -62,18 +62,6 @@ ActiveRecord::Schema.define(version: 2018_09_12_161038) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
-  create_table "admins", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
-
   create_table "artists", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
